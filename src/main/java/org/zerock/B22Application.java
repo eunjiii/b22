b34e2357e7a6b5a39272bd939f537ce64fc2f43d
eunjiii <a01049470155@gmail.com>
2018-11-08 16:45:37 +0900

    123

---
+++ src/main/java/org/zerock/B22Application.java
@@ -0,0 +1,12 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B22Application.class, args);
+	}
+}
---
+++ src/main/java/org/zerock/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author 5CLASS-184
+ *
+ */
+package org.zerock.controller;
